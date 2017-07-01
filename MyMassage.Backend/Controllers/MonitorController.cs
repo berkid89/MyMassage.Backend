11ebdbf743c70f95a5dffe9d8129f3a079ad25c8
berkid89 <berkid89@gmail.com>
2017-07-01 18:33:34 +0200

    initial

---
+++ MyMassage.Backend/Controllers/MonitorController.cs
@@ -0,0 +1,14 @@
+﻿using Squire.Monitoring.Controllers;
+using System;
+using System.Threading.Tasks;
+
+namespace MyMassage.Backend.Controllers
+{
+    public class MonitorController : MonitoringController
+    {
+        public override Task<bool> HealthCheck()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
